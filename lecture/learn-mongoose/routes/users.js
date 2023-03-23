599b9d735d003456d5d4d3255e3b467185cdbccf
sudoku-jm <sudoku8386@gmail.com>
2023-03-23 16:50:25 +0900

    몽구스 실전 프로젝트

---
+++ lecture/learn-mongoose/routes/users.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const User = require('../schemas/user');
+const Comment = require('../schemas/comment');
+
+const router = express.Router();
+
+router.route('/')
+  .get(async (req, res, next) => {
+    try {
+      const users = await User.find({});
+      res.json(users);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  })
+  .post(async (req, res, next) => {
+    try {
+      // 방법1
+      const user = await User.create({
+        name: req.body.name,
+        age: req.body.age,
+        married: req.body.married,
+      });
+
+      // 방법2
+      // const user = new User({
+      //   name: req.body.name,
+      //   age: req.body.age,
+      //   married: req.body.married,
+      // });
+      // await user.save();
+
+      console.log(user);
+      res.status(201).json(user);
+    } catch (err) {
+      console.error(err);
+      next(err);
+    }
+  });
+
+router.get('/:id/comments', async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ commenter: req.params.id })
+      .populate('commenter');
+    console.log(comments);
+    res.json(comments);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+module.exports = router;
