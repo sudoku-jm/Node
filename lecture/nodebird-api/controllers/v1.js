aa4da5944d4336e6fa83e37ffa4885e39221ff6a
sudoku-jm <sudoku8386@gmail.com>
2023-04-19 21:07:56 +0900

    API요청 서버 날리기

--- lecture/nodebird-api/controllers/v1.js
+++ lecture/nodebird-api/controllers/v1.js
@@ -1,53 +1,43 @@
 const jwt = require('jsonwebtoken');
-const {Domain, User} = require('../models');
+const { Domain, User } = require('../models');
 
-
-//토큰 발급 라우터
-exports.createToken = async(req,res) => {
-    const {clientSecret} = req.body;
-    try{
-        // 도메인 등록 검사. 요금제, 할당량 등 검사.
-        const domain = await Domain.findOne({
-            where : { clientSecret },
-            include : {
-                model : User,
-                attribute : ['id','nick']
-            },
-        });
-
-        if(!domain){
-            return res.status(401).json({
-                code : 401,
-                message : '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요.'
-            });
-        }
-
-        // 토큰 발급.  앞으로 발급 받은 토큰을 사용해서 API 서버에 요청할 것이다.
-        const token = jwt.sign({
-            id : domain.Uesr.id,
-            nick : domain.User.nick,
-        }, process.env.JWT_SECRET, {
-            expiresIn:'1m',      //1분 만료기간 
-            issuer:'nodebird',   //발급자
-        });
-
-        return res.json({
-            code : 200,
-            message : '토큰이 발급되었습니다.',
-            token,
-        })
-
-    }catch(err){
-        console.error(err);
-        return res.status(500).json({
-            code : 500,
-            message : '서버 에러',
-        })
+exports.createToken = async (req, res) => {
+  const { clientSecret } = req.body;
+  try {
+    const domain = await Domain.findOne({
+      where: { clientSecret },
+      include: {
+        model: User,
+        attribute: ['nick', 'id'],
+      },
+    });
+    if (!domain) {
+      return res.status(401).json({
+        code: 401,
+        message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
+      });
     }
-}
+    const token = jwt.sign({
+      id: domain.User.id,
+      nick: domain.User.nick,
+    }, process.env.JWT_SECRET, {
+      expiresIn: '1m', // 1분
+      issuer: 'nodebird',
+    });
+    return res.json({
+      code: 200,
+      message: '토큰이 발급되었습니다',
+      token,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      code: 500,
+      message: '서버 에러',
+    });
+  }
+};
 
-
-// 토큰 발급 테스트
 exports.tokenTest = (req, res) => {
-    res.json(res.locals.decoded);
-}
+  res.json(res.locals.decoded);
+};
--- lecture/nodebird-api/middlewares/index.js
+++ lecture/nodebird-api/middlewares/index.js
@@ -21,19 +21,19 @@ exports.isNotLoggedIn = (req, res, next) => {
 };
 
 exports.verifyToken = (req, res, next) => {
-    try{
-        res.locals.decoded = jwt.verify(req.headers.authorization, prosess.env.JWT_SECRET);
-        return next();
-    }catch(err){
-        if(err.name === 'TokenExpriedError'){   //유효기간 초과
-            return res.status(419).json({
-                code : 419,
-                message : '토큰이 만료되었습니다.',
-            })
-        }
-        return res.status(401).json({
-            code : 401,
-            message : '유효하지 않은 토큰입니다',
+    try {
+      res.locals.decoded = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+      return next();
+    } catch (err) {
+      if (err.name === 'TokenExpiredError') { // 유효기간 초과
+        return res.status(419).json({
+          code: 419,
+          message: '토큰이 만료되었습니다',
         });
-    }   
-}
+      }
+      return res.status(401).json({
+        code: 401,
+        message: '유효하지 않은 토큰입니다',
+      });
+    }
+  };
